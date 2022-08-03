2b8bd0fdea309c9c3d5ef299f55f69b3362e9c44
RITIK900 <80908112+RITIK900@users.noreply.github.com>
2022-08-03 18:39:56 +0530

    Commit

--- test.cpp
+++ test.cpp
@@ -1,6 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    printf("\\\\");
+int main()
+{
+    int n;
+    printf("Which table do you want to print on the screen : ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= 10; i++)
+        printf("%d * %d = %d\n", n, i, n * i);
+
     return 0;
 }
