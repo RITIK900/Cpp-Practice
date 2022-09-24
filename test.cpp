bef026672c8e41d6d1a2ab678e55aedfd18a8a53
RITIK900 <80908112+RITIK900@users.noreply.github.com>
2022-09-24 09:05:39 +0530

    UPdate

--- test.cpp
+++ test.cpp
@@ -1,26 +1,78 @@
 #include <iostream>
 using namespace std;
 
-#define PI 3.14
-float area(float r)
+class Complex
 {
-    return PI*r*r;
+private:
+    /* data */
+    int real;
+    int img;
+public:
+    Complex(/* args */);
+
+    Complex(int a, int b)
+    {
+        real=a;img=b;
+    }
+
+    void display()
+    {
+        cout<<real<<" + i"<<img;
+    }
+
+    Complex operator+(Complex c)
+    {
+        Complex temp;
+        temp.real= real+c.real;
+        temp.img= img+c.img;
+        return temp;
+    }
+
+    Complex operator-(Complex c)
+    {
+        Complex temp;
+        temp.real= real-c.real;
+        temp.img= img-c.img;
+        return temp;
+    }
+
+    Complex operator*(Complex c)
+    {
+        Complex temp;
+        temp.real= real*c.real;
+        temp.img= img*c.img;
+        return temp;
+    }
+
+    bool operator==(Complex c)
+    {
+        if(real == c.real && img == c.img)
+         return true;
+
+        return false;
+    }
+
+    friend 
+
+    ~Complex();
+};
+
+Complex::Complex(/* args */)
+{
 }
-int area(int l,int b)
+
+Complex::~Complex()
 {
-    return l*b;
-}
-float area(float base, float height)
-{
-    return 0.5*base*height;
 }
 
 
 int main()
 {
-    cout<<"Area of the circle is "<<area(1)<<endl;
-    cout<<"Area of the Rectangle is "<<area(2,3)<<endl;
-    cout<<"Area of the Triangle is "<<area(2.0,3.0)<<endl;
+    Complex c1(1,2);
+    Complex c2(3,4);
+
+    cout<<c1*c2;
+    cout<<(c1==c2);
 
     return 0;
 }
